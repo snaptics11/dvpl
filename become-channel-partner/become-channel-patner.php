89fda22c6d7d3e45715a6ef0f4f0757ece0ad533
koushiksai166 <saikoushik166@gmail.com>
2025-07-07 14:55:52 +0530

    updated version

---
+++ become-channel-partner/become-channel-patner.php
@@ -0,0 +1,46 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["your-name"]));
+    $email = filter_var(trim($_POST["your-email"]), FILTER_SANITIZE_EMAIL);
+    $phone = strip_tags(trim($_POST["tel-496"]));
+    $message = strip_tags(trim($_POST["your-Message"]));
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'dvpldigitalmarketing@gmail.com';
+        $mail->Password = 'wlon yvqb tome rrvh';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        // Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('laxmareddy.j@dvpl.org');
+        $mail->addAddress('ramesh@dvpl.in');
+        $mail->addAddress('rpreethikareddy@dvpl.org');
+
+        // Content
+        $mail->isHTML(false);
+        $mail->Subject = "New Become Channel Partner Submission from $name";
+        $mail->Body = "Name: $name\nEmail: $email\nPhone:  $message\nMessage:";
+
+        $mail->send();
+        echo json_encode(["success" => true, "message" => "Thank you for contacting us, $name. We will get back to you soon!"]);
+    } catch (Exception $e) {
+        echo json_encode(["success" => false, "message" => "Oops! Something went wrong. Please try again later. Mailer Error: {$mail->ErrorInfo}"]);
+    }
+} else {
+    echo json_encode(["success" => false, "message" => "There was a problem with your submission. Please try again."]);
+}
+?>
